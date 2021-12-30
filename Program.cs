75ad5b4aece5e4c4ea6353d0c5c478e975ac47b6
CSmith-7652 <96793973+CSmith-7652@users.noreply.github.com>
2021-12-30 14:19:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(" Fun With Async ===>");
+            //Console.WriteLine(DoWork());
+            string message = await DoWorkAsync();
+            Console.WriteLine(message);
+            Console.WriteLine("Completed");
+            Console.ReadLine();
+        }
+
+        static string DoWork()
+        {
+            Thread.Sleep(5_000);
+            return "Done with work!";
+        }
+
+        static async Task<string> DoWorkAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(5_000);
+                return "Done with work!";
+            });
+        }
+    }
+}
